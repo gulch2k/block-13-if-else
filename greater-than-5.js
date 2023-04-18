6d2f6fc5493f0d571554e9c0d146d00e9c30f8be
jamaal <palacebikes@gmail.com>
2023-04-17 20:47:17 -0700

    added code to the file

---
+++ greater-than-5.js
@@ -0,0 +1,13 @@
+// using if/else statements, using number values, num1 and num2.
+// if both numbers are greater than 5 or equal to, print "true"
+// else print "false"
+
+let num1= 5;
+
+let num2= 6;
+
+if (num1 > 5 || num2 >= 5){
+    console.log("true");
+}
+else 
+    console.log("false");
---
+++ number-line.js
@@ -0,0 +1,25 @@
+//If the sum is less than -1000, print "is less than -1000";
+// If the sum is a negative number, print "is a negative number";
+// if the sum is equal to 0, print "is equal to 0";
+// if the sum is larger than 0, print "is larger than 0";
+// if the sum is greater than 100, print "is greater than 100";
+
+let num1 = -5;
+
+let num2 = 0;
+
+let sum = num1 + num2;
+
+  if (sum < -1000) {
+    console.log("is less than -1000");
+  } else if (sum < 0) {
+    console.log("is a negative number");
+  } else if (sum === 0) {
+    console.log("is equal to 0");
+  } else if (sum > 0) {
+    console.log("is larger than 0");
+  } else if (sum > 100) {
+    console.log("is greater than 100");
+  } else {
+    console.log("is not a number");
+  }
